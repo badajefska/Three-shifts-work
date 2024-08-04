4621c3b0f521668cdd978c47fff8bbbd8d120d97
Badajefska <100071413+badajefska@users.noreply.github.com>
2024-08-04 01:50:55 +0000

    Update PythonApplication1.py
    hi!

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,25 +1,23 @@
 from datetime import datetime
 
-dt = datetime.today()
-d = dt.date()
-print('please press day example ' + str(d))
-day = input()
-print("search of the", day)
+def calculate_shift(day):
+    dtday = datetime.strptime(day,'%Y-%m-%d') -datetime(1899,12,31)
+    serial = dtday.days + 1
+    char = hex(serial + 6)
+    char02 = char[-1]
 
-dtday = datetime.strptime(day, '%Y-%m-%d') - datetime(1899, 12, 31)
-serial = dtday.days + 1
-print(serial)
-#Add to the flow of each group
-print(serial+ 6)
-print(hex(serial+ 6))
-char = hex(serial+6)
-length = len(hex(serial+6))
-char02 = char[length - 1]
-print(char02)
+    threeshiftswork = {
+            "1": "甲①", "2": "甲②", "3": "甲③", "4": "甲④", "5": "休",
+        "6": "乙①", "7": "乙②", "8": "乙③", "9": "乙④", "a": "休",
+        "b": "丙①", "c": "丙②", "d": "丙③", "e": "丙④", "f": "休", "0": "休",
+    }
 
-threeshiftswork = {"1":"甲①","2":"甲②","3":"甲③","4":"甲④","5":"休","6":"乙①","7":"乙②","8":"乙③","9":"乙④","a":"休","b":"丙①","c":"丙②","d":"丙③","e":"丙④","f":"休","0":"休",}
+    if char02 in threeshiftswork:
+        return threeshiftswork[char02]
+    else:
+        return None
 
-if char02 in threeshiftswork:
-    val = threeshiftswork[char02]
-    print(val)
-    
+day = input('Please enter a date (YYYY-MM-DD): ')
+shift = calculate_shift(day)
+print(f"The shift for {day} is : {shift}")
+
